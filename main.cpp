bbbf1cfcc7c5ace22b063b7bd5a34d114f3d6082
RiccardoSambito <117939705+RiccardoSambito@users.noreply.github.com>
2022-11-23 19:05:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
    int n=0, a=1, b=1, c=0
-      cin>>n
+      cin>>n;
       if(n>=2){
          cout<<a<<endl<<b<<endl;
          for (int i=2; i<n; i++){
@@ -12,6 +12,9 @@ int main() {
             b=c;
             cout<<c<<endl;
          }
+      }
+      else{
+         cout<<"errore"<<endl;
       }
    return 0;
 }
